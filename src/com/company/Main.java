55a4dc4181359b7058f939b28aebd4760544c47c
vivi <vivi@iMac-de-jeronimo.local>
2022-08-12 09:48:23 -0300

    Ejercicio parte 1 y 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,22 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int resultado=0;
-        resultado=suma(3,4,5);
-        System.out.println(resultado);
+       int resultado=0;
+       resultado=suma(3,4,5);
+       System.out.println(resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuerta();
+        System.out.println(miCoche.puertas);
     }
     public static int suma(int a, int b, int c){
         return (a+b+c);
+    }
+}
+class Coche {
+    public int puertas = 0;
+
+    public void AgregarPuerta(){
+        this.puertas ++;
     }
 }
